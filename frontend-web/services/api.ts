6949129fa5ef42d9a6528ff1d9d0a715e5764b84
Aunkko-0 <tanapol1108@gmail.com>
2026-02-11 11:34:01 +0700

    api.ts config

--- frontend-web/services/api.ts
+++ frontend-web/services/api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import type { InternalAxiosRequestConfig } from "axios";
 
 export const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
@@ -7,11 +8,12 @@ export const api = axios.create({
     },
 });
 
-api.interceptors.request.use((config) => {
+// 2. ระบุ Type ให้กับ parameter 'config'
+api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
     const token = localStorage.getItem("token");
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
-  }
-  
-  return config;
+    }
+    
+    return config;
 });
